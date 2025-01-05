6739aad9a905e14ae6f412f447f2eb7c5f72a01f
malcolmkmd <hi@malcolmk.com>
2025-01-05 12:10:22 +0200

    Initial login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <>
+      <div className='flex h-screen w-full items-center justify-center px-4'>
+        <Card className='max-w-sm'>
+          <CardHeader>
+            <CardTitle className='text-2xl'>Login</CardTitle>
+            <CardDescription>Enter your email below to login to your account</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form className='flex flex-col gap-y-4'>
+              <div className='flex flex-col gap-y-2'>
+                <Label>Email</Label>
+                <Input placeholder='hello@email.com'/>
+              </div>
+              <Button>Submit</Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   )
 }
